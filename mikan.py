51d2d85c03e77ca25176fa0f8e1cfb567eb52954
JIe-n <service.azazel.bot@gmail.com>
2020-11-17 21:46:36 +0300

    Update mikan.py
    Co-Authored-By: JIe-na <74618563+jie-na@users.noreply.github.com>

--- mikan.py
+++ mikan.py
@@ -4,7 +4,7 @@ import json
 import requests
 import random
 
-verify = False
+
 requests.packages.urllib3.disable_warnings()
 
 
@@ -28,7 +28,7 @@ async def on_message(message):
     if message.author == bot.user:
         return'''
 
-bot.event
+@bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CommandNotFound):
         await ctx.send('Неизвестная команда. Проверь правильность написания или напиши "!help" для помощи')
@@ -51,49 +51,49 @@ async def randpict(ctx):
 
 @randpict.command() #редачим вторую часть команды ranpict
 async def dog(ctx): #проверка того, что написали
-    response = requests.get('https://some-random-api.ml/img/dog') #просим то, что находится в сайте
+    response = requests.get('https://some-random-api.ml/img/dog', verify = False) #просим то, что находится в сайте
     json_data = json.loads(response.text) #извлекаем json
     embed = discord.Embed(color = 0xa34f00, title = 'Случайная собачка') #создаём embed, цвет коричневый
     embed.set_image(url = json_data['link']) #делаем наш json как картинку в embed-е
     await ctx.send(embed = embed) #отправляем embed
 @randpict.command() #повторяем для всех животных
 async def cat(ctx): 
-    response = requests.get('https://some-random-api.ml/img/cat') 
+    response = requests.get('https://some-random-api.ml/img/cat', verify = False) 
     json_data = json.loads(response.text) 
     embed = discord.Embed(color = 0x8000ff, title = 'Случайная котя :3') #цвет - фиолетовый
     embed.set_image(url = json_data['link']) 
     await ctx.send(embed = embed)
 @randpict.command() 
 async def red_panda(ctx): 
-    response = requests.get('https://some-random-api.ml/img/red_panda') 
+    response = requests.get('https://some-random-api.ml/img/red_panda', verify = False) 
     json_data = json.loads(response.text) 
     embed = discord.Embed(color = 0xff0000, title = 'Красная панда?..') #цвет - красный
     embed.set_image(url = json_data['link']) 
     await ctx.send(embed = embed)
 @randpict.command()
 async def panda(ctx): 
-    response = requests.get('https://some-random-api.ml/img/panda') 
+    response = requests.get('https://some-random-api.ml/img/panda', verify = False) 
     json_data = json.loads(response.text) 
     embed = discord.Embed(color = 0xffffff, title = 'Случайная панда')  #цвет - белый
     embed.set_image(url = json_data['link']) 
     await ctx.send(embed = embed)
 @randpict.command() 
 async def birb(ctx): 
-    response = requests.get('https://some-random-api.ml/img/birb') 
+    response = requests.get('https://some-random-api.ml/img/birb', verify = False) 
     json_data = json.loads(response.text) 
     embed = discord.Embed(color = 0xffee00, title = 'Случайная птица', folder = 'Ни один RHM Птица не пострадал') #цвет - жёлтый
     embed.set_image(url = json_data['link']) 
     await ctx.send(embed = embed)
 @randpict.command() 
 async def fox(ctx): 
-    response = requests.get('https://some-random-api.ml/img/fox') 
+    response = requests.get('https://some-random-api.ml/img/fox', verify = False) 
     json_data = json.loads(response.text) 
     embed = discord.Embed(color = 0xff9900, title = 'Случайная лисичка') #цвет - оранжевый
     embed.set_image(url = json_data['link']) 
     await ctx.send(embed = embed)
 @randpict.command() 
 async def koala(ctx): 
-    response = requests.get('https://some-random-api.ml/img/koala') 
+    response = requests.get('https://some-random-api.ml/img/koala', verify = False) 
     json_data = json.loads(response.text) 
     embed = discord.Embed(color = 0x878787, title = 'Случайная коала') #цвет - серый
     embed.set_image(url = json_data['link']) 
